8f5f18c798ca6ab4093b1f12a2be02011c84fc57
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-31 01:08:22 -0500

    model db

--- database/db.sql
+++ database/db.sql
@@ -2,6 +2,7 @@ CREATE DATABASE database_links;
 
 USE database_links;
 
+-- USERS TABLE
 CREATE TABLE users(
   id INT(11) NOT NULL,
   username VARCHAR(16) NOT NULL,
@@ -13,6 +14,25 @@ ALTER TABLE users
   ADD PRIMARY KEY (id);
 
 ALTER TABLE users
-  MODIFY id INIT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+  MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 DESCRIBE users;
+
+-- LINKS TABLE
+CREATE TABLE links (
+  id INT(11) NOT NULL,
+  title VARCHAR(150) NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  description TEXT,
+  user_id INT(11),
+  created_at timestamp NOT NULL DEFAULT current_timestamp,
+  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+ALTER TABLE links
+  ADD PRIMARY KEY (id); 
+
+ALTER TABLE links
+  MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE links;
