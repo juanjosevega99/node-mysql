8788cbd67df936f1fb81b806fb3d36f6b7f3678e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-30 23:25:08 -0500

    views ready

--- src/index.js
+++ src/index.js
@@ -13,8 +13,10 @@ app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   layoutsDir: path.join(app.get('views'), 'layouts'),
   partialsDir: path.join(app.get('views'), 'partials'),
-  extname: '.hbs'
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
 }))
+app.set('view engine', '.hbs')
 
 // middlewares
 app.use(morgan('dev'))
