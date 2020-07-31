9617af6f5fb3f8f7043298a5b94dccdeed825113
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-30 23:22:07 -0500

    define .hbs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const path = require('path')
 
 // initializations
 const app = express()
 
 // settings
 app.set('port', process.env.PORT || 4000)
+app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', exphbs({
-  defaultLayout: 'main'
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
 }))
 
 // middlewares
