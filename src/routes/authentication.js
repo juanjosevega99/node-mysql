55258cf848c2f107afad98f2868eb7c1735152f0
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-01 18:59:52 -0500

    isloggedIn

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('passport')
-// const { isLoggedIn } = require('../lib/auth')
+const { isLoggedIn } = require('../lib/auth')
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup')
