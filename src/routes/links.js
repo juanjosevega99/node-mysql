933ac978c3ac7dcecdd5a6a15d87c19f6374f2d8
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-01 10:45:55 -0500

    Dabase is saving

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,8 +7,15 @@ router.get('/add', (req, res) => {
   res.render('links/add')
 })
 
-router.post('/add', (req, res) => {
-  res.render('recived')
+router.post('/add', async (req, res) => {
+  const { title, url, description } = req.body
+  const newLink = {
+    title,
+    url,
+    description
+  }
+  await pool.query('INSERT INTO links set ?', [newLink])
+  res.send('received')
 })
 
 module.exports = router
