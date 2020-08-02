96d07b57ce99d50bdae89403a991fafc480e7a79
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-01 19:07:28 -0500

    routes necesary to watch

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,22 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('passport')
-const { isLoggedIn } = require('../lib/auth')
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth')
 
-router.get('/signup', (req, res) => {
+router.get('/signup', isNotLoggedIn, (req, res) => {
   res.render('auth/signup')
 })
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
   successRedirect: '/profile',
   failureRedirect: '/signup',
 }))
 
-router.get('/signin', (req, res) => {
+router.get('/signin', isNotLoggedIn, (req, res) => {
   res.render('auth/signin')
 })
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local.signin', {
     successRedirect: '/profile',
     failureRedirect: '/signin',
@@ -28,7 +28,7 @@ router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile')
 })
 
-router.get('/logout', (req, res) => {
+router.get('/logout', isNotLoggedIn, (req, res) => {
   req.logOut();
   res.redirect('/signin');
 });
